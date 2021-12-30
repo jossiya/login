6b880439c22906143f543764fec5e527dbbda50f
조정민 <rolety2202@naver.com>
2021-12-30 21:09:09 +0900

    first commit

---
+++ routes/home/home.ctrl.js
@@ -0,0 +1,14 @@
+"use script"
+
+const home =(req, res)=>{
+    res.render("home/index");
+};
+
+const login = (req, res)=>{
+    res.render("home/login");
+}; 
+
+module.exports ={
+    home,
+    login,
+};
