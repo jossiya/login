9c74021d0b322733a5e8afc958e268399c3c882f
조정민 <rolety2202@naver.com>
2021-12-30 21:39:32 +0900

    다시 또올리기

--- app/app.js
+++ app/app.js
@@ -3,10 +3,10 @@ const express =require("express");
 const app =express();
 
 //라우팅
-const home=require("./routes/home");
+const home=require("./src/routes/home");
 
 //앱 셋팅
-app.set("views","./views");
+app.set("views","./src/views");
 app.set("view engine", "ejs");
 
 app.use("/", home); //->미들 웨어를 등록해주는 메서드.
